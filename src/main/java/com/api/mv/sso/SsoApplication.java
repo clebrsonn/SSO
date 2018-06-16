4eff5a6cdc10b36c3c901c3cf44e364104354637
cleberson <clebrsonn@hotmail.com>
2018-06-16 17:23:16 -0300

    no message

--- src/main/java/com/api/mv/sso/SsoApplication.java
+++ src/main/java/com/api/mv/sso/SsoApplication.java
@@ -1,4 +1,4 @@
-package com.api.mv;
+package com.api.mv.sso;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
@@ -6,7 +6,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class SsoApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(SsoApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(SsoApplication.class, args);
+    }
 }
--- src/test/java/com/api/mv/sso/SsoApplicationTests.java
+++ src/test/java/com/api/mv/sso/SsoApplicationTests.java
@@ -1,4 +1,4 @@
-package com.api.mv;
+package com.api.mv.sso;
 
 import org.junit.Test;
 import org.junit.runner.RunWith;
@@ -9,8 +9,8 @@ import org.springframework.test.context.junit4.SpringRunner;
 @SpringBootTest
 public class SsoApplicationTests {
 
-	@Test
-	public void contextLoads() {
-	}
+    @Test
+    public void contextLoads() {
+    }
 
 }
